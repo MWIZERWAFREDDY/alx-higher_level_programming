299216484060c2ececc41a1a9e29f19259e29d2d
FREDDY MWIZERWA <57982668+MWIZERWAFREDDY@users.noreply.github.com>
2022-06-20 23:11:39 +0200

    try except

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+#excpt error
+def raise_exception():
+    raise TypeError
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+#raise
+def raise_exception_msg(message=""):
+    raise NameError(message)
